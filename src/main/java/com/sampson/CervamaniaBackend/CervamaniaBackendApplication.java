92693f60b0c84426bf0b0966cfa475eccb01a0e7
fsampson30 <fsampson30@gmail.com>
2022-12-19 07:12:01 -0300

    Initial commit

---
+++ src/main/java/com/sampson/CervamaniaBackend/CervamaniaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sampson.CervamaniaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CervamaniaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CervamaniaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sampson/CervamaniaBackend/CervamaniaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.CervamaniaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CervamaniaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
